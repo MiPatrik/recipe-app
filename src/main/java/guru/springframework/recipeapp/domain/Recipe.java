04a99d4c3bda85b719cb3c3224c630cfca88469b
Patrik Miculka <MiPatrik@gmail.com>
2019-03-11 21:30:57 +0100

    added entity repositories

--- src/main/java/guru/springframework/recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeapp/domain/Recipe.java
@@ -16,16 +16,19 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //    private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @Lob
     private byte[] image;
-
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
-
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
-
 
     public Long getId() {
         return id;
@@ -91,6 +94,14 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public byte[] getImage() {
         return image;
     }
@@ -105,6 +116,14 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     public Set<Ingredient> getIngredients() {
